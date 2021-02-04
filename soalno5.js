b164ccb64fda9921132e724fb5ab710563df5362
Brianrahmarela <brian.rahmarela@gmail.com>
2021-02-04 11:36:22 +0700

    first commit

--- soalno5.js
+++ soalno5.js
@@ -1,24 +1,33 @@
-//buat variable tampung
-let tampung = [];
-
 function rangeAngka(times,number){
-//   console.log(times,number);
-
+  // console.log(times,number);
+  //buat var penampung arr
+  let tampung = [];
+  
   if(times <= 0){
-    return "";
+    return [];
   } else {
-    // console.log(number);
     // console.log(times);
-    // console.log(tampung.push(number));
+
+    //assign variable tampung dgn value rangeAngka(times-1,number)
+    //yg nantinya akan didapat value numbernya, function rangeAngka(times-1,number)
+    //untuk memperkecil index time (times-1)
+    tampung = rangeAngka(times-1,number);
     // console.log(tampung);
+    // console.log(times);
+
+    //kemudian ambil valuenya mskkan ke array dgn push
     tampung.push(number);
-    rangeAngka(times-1,number);
+    // console.log(times);
+    // console.log(number);
+    // tampung.push(number)
     return tampung
+    
   }
+
 }
 
-// console.log(rangeAngka(0,3)); //[]
-// console.log(rangeAngka(-1,5)); //[]
-// console.log(rangeAngka(1,4)); //[4]
-// console.log(rangeAngka(2,3));//[4,3,3]
-console.log(rangeAngka(5,7));
+console.log(rangeAngka(0,3));
+console.log(rangeAngka(-1,5));
+console.log(rangeAngka(1,4)); //[4]
+console.log(rangeAngka(2,3)); //[4]
+console.log(rangeAngka(5,7)); 
